5b1b92656aa5cd89d52963eda8ab05ecf8c82675
gildeonbs <gildeon23@gmail.com>
2025-09-21 22:45:09 -0300

    Spring Boot project initialized

---
+++ backend/src/main/java/com/github/gildeonbs/strivexp/StriveXpApplication.java
@@ -0,0 +1,13 @@
+package com.github.gildeonbs.strivexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriveXpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriveXpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/gildeonbs/strivexp/StriveXpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gildeonbs.strivexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StriveXpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
