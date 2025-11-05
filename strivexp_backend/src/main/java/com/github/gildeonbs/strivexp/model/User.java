15ea67c677de44b8c38d5c50cc004689d0f35df3
gildeonbs <gildeon23@gmail.com>
2025-11-05 01:35:14 -0300

    Config and model packages criation

---
+++ strivexp_backend/src/main/java/com/github/gildeonbs/strivexp/model/User.java
@@ -0,0 +1,60 @@
+package com.strivexp.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "display_name")
+    private String displayName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(name = "birthday_date", nullable = false)
+    private LocalDate birthdayDate;
+
+    private String avatar;
+
+    private String timezone = "UTC";
+
+    private String locale = "us_EN";
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+}
+
