2be14189237595f8d0d3b6da036e4a8224004b7c
gildeonbs <gildeon23@gmail.com>
2026-01-10 13:06:04 -0300

    Implement Swagger and OpenAPI Documentation

---
+++ strivexp_backend/src/main/java/com/github/gildeonbs/strivexp/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.github.gildeonbs.strivexp.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "StriveXP Team",
+                        email = "gildeon23@gmail.com"
+                ),
+                description = "OpenAPI documentation for StriveXP Backend",
+                title = "StriveXP API",
+                version = "1.0"
+        ),
+        // Applies this security requirement globally to all endpoints
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
