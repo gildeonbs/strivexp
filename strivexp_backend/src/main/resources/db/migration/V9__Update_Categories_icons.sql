3116c4dbcf57dd8bb1cf7fee0b8181442ecec0fd
gildeonbs <gildeon23@gmail.com>
2026-01-15 11:17:45 -0300

    Update Flyway Migration .sql file for Categories icon adjust

---
+++ strivexp_backend/src/main/resources/db/migration/V9__Update_Categories_icons.sql
@@ -0,0 +1,11 @@
+-- =========================================================
+-- Update categories icon
+-- =========================================================
+
+UPDATE categories SET icon = '🧠' WHERE code = 'MND';
+UPDATE categories SET icon = '💪' WHERE code = 'HLT';
+UPDATE categories SET icon = '💛' WHERE code = 'EMP';
+UPDATE categories SET icon = '🧘' WHERE code = 'MMW';
+UPDATE categories SET icon = '🎨' WHERE code = 'CRT';
+UPDATE categories SET icon = '🌍' WHERE code = 'ENV';
+
