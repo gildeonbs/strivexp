c31325d634e6d09b4516c3caba762022947373bd
gildeonbs <gildeon23@gmail.com>
2026-01-20 13:25:27 -0300

    Deploy Settings

--- strivexp_mobile/lib/core/constants/api_constants.dart
+++ strivexp_mobile/lib/core/constants/api_constants.dart
@@ -1,7 +1,8 @@
 class ApiConstants {
 
-  static const String baseUrl = 'http://10.0.2.2:8080';
   //static const String baseUrl = 'https://shade-perry-bomb-breakfast.trycloudflare.com' // Placeholder
+  //static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'https://strivexp.onrender.com';
 
   static const String loginEndpoint = '/api/auth/login';
   static const String passwordResetRequestEndpoint = '/api/auth/password-reset/request';
